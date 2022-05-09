c6fd2b7ff0da528baaf6722b2694f667c861e6fb
Premasis <sahupremasis@yahoo.com>
2022-05-09 10:25:49 +0800

    Updated Codes

--- webpack-project/webpack.config.js
+++ webpack-project/webpack.config.js
@@ -13,12 +13,19 @@ module.exports = {
   mode: mode,
   target: target,
 
+  output: {
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+
   module: {
     rules: [
       {
         test: /\.(s[ac]|c)ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {publicPath: ""}
+          },
           "css-loader",
           "postcss-loader",
           "sass-loader",
