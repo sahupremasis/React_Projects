113ba3b650d2fc45028f4d66e17e84a656ca5254
Premasis <sahupremasis@yahoo.com>
2022-07-09 11:54:37 +0800

    Updated webpack project

--- webpack-project/src/components/App.jsx
+++ webpack-project/src/components/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       <section className="hero"></section>
       <main>
         <section>
-          <h1>Oh Herro, React.</h1>
+          <h1>Oh Herro, React!!</h1>
           <img src={sword} alt="sword" width="250" />
           <img src={swordSvg} alt="sword" width="250" />
           <Recipes />
